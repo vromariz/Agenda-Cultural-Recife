7db67aef6b320e25ce965d82ac28cf53e990b2f0
vromariz <viniciusrfscabral@gmail.com>
2025-10-16 21:01:20 -0300

    adicionei o spring boot

---
+++ agenda-cultural/src/main/java/br/recife/agenda/AgendaCulturalApplication.java
@@ -0,0 +1,13 @@
+package br.recife.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCulturalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaCulturalApplication.class, args);
+        System.out.println("🚀 Servidor Spring Boot rodando em http://localhost:8080");
+    }
+}
