067ef2adbeb570fd70c1b6a9c1621c13f7fdc482
vromariz <viniciusrfscabral@gmail.com>
2025-10-21 00:28:39 -0300

    banco de dados configurado

--- agenda-cultural/src/main/java/br/recife/agenda/user/controller/HomeController.java
+++ agenda-cultural/src/main/java/br/recife/agenda/user/controller/HomeController.java
@@ -1,4 +1,4 @@
-package br.recife.agenda.controller;
+package br.recife.agenda.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
